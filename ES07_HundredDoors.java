f45368269298fafc7c0d6bb9075156047dfca81d
Christian Lagelstorfer <wi19b076@technikum-wien.at>
2019-11-12 02:53:10 +0100

    Upload New File

---
+++ ES07_HundredDoors.java
@@ -0,0 +1,29 @@
+package HundredDoors;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        System.out.printf(": ");
+		int a=sc.nextInt();
+		boolean doors[]=new boolean[a+1];
+				
+		 for (int i = 1; i < doors.length; i++) {
+	            for (int j = i; j < doors.length; j += i) {
+	                doors[j] = !doors[j];
+	            }
+	        }
+	 
+	        for (int i = 0; i < doors.length; i++) {
+	            //if (doors[i]) {
+	                System.out.printf("\n%d "+doors[i], i);
+	            //}
+	        }
+			sc.close();
+
+		}
+
+	}
+
+
