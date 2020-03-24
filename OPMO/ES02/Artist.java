27b21b63e93395185122716684f23aee45a5ec79
Christian Lagelstorfer <wi19b076@technikum-wien.at>
2020-03-25 00:24:16 +0100

    Upload New File

---
+++ OPMO/ES02/Artist.java
@@ -0,0 +1,27 @@
+package MusicLandscape.entities;
+
+public class Artist
+
+{
+	private String name;
+	
+	public Artist() {
+		name="unknown";
+	}
+	public Artist(Artist a) {
+		name = a.name;
+	}
+	public Artist (String name) {
+		//if (name != null && name.trim().length() > 0)
+			this.name=name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		if (name != null && name.trim().length() > 0)
+			this.name = name;
+	}
+}
